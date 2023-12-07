df747fb7e7b38b32b0f5b1df824696e86cd469d2
Anna Kozik <anna.kozik@yahoo.pl>
2023-12-07 14:58:29 +0100

    ref: add changes to page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import { PropertyEditor } from "@/components/PropertyEditor";
 
 export default function Home() {
   return (
-    <main className="flex h-full  p-4 pb-[56px]">
-      <div className="flex justify-center items-center w-3/5 bg-red-200">
-        <div className="relative flex justify-center items-center h-4/5 xl:h-full aspect-square bg-green-200">
-          {/* Place for vase image component */}
+    <main className="flex justify-center h-full p-4 pb-[40px]">
+      <div className="flex w-full 2xl:w-[1500px]">
+        <div className="flex justify-center items-center w-3/5 bg-red-200">
+          <div className="relative flex justify-center items-center w-[calc(85vh)] 2xl:w-full aspect-square bg-green-200">
+            Place for vase image component
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center items-start w-2/5 ">
-        {/* Place for Tabs Component */}
-        <PropertyEditor />
+        <div className="flex justify-center items-start w-2/5 pl-4">
+          {/* Place for Tabs Component */}
+          <PropertyEditor />
+        </div>
       </div>
     </main>
   );
