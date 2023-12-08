4814de37b6e62e5c546a39205aae7473c0057dc6
Anna Kozik <anna.kozik@yahoo.pl>
2023-12-08 17:54:05 +0100

    style: ProfilePointForm styling

--- src/components/ProfilePointForm/index.tsx
+++ src/components/ProfilePointForm/index.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
 
 export const ProfilePointForm = () => {
   return (
@@ -11,26 +12,34 @@ export const ProfilePointForm = () => {
         id="color"
         className="h-[33px] aspect-square bg-red-500 border border-black"></div>
       <div id="inputs&slidersContainer" className="flex flex-col gap-2 px-2">
-        <div className="flex items-center justify-between gap-6 ">
-          <Label>Points</Label>
-          <Input className="h-9 w-16" />
+        <div className="flex items-center justify-between gap-6">
+          <Label htmlFor="points">Points</Label>
+          <Input id="points" className="h-7 w-24 border-slate-300" />
         </div>
-        {/* <div className="flex">
-          <label>Angle start</label>
-          <Input />
+        <div className="flex items-center justify-between gap-6">
+          <Label htmlFor="offset">Offset</Label>
+          <Input id="offset" className="h-7 w-24 border-slate-300" />
         </div>
-        <div className="flex">
-          <label>Angle step</label>
-          <Input />
+        <div className="flex items-center justify-between gap-6">
+          <Label htmlFor="angleStart">Angle start</Label>
+          <Input id="angleStart" className="h-7 w-24 border-slate-300" />
         </div>
-        <div>
-          <label>Easing in</label>
-          {/* <Slider /> */}
-        {/* </div>
-        <div>
-          <label>Easing out</label>
-          {/* <Slider /> */}
-        {/*} </div> */}
+        <div className="flex items-center justify-between gap-6">
+          <Label htmlFor="angleStep">Angle step</Label>
+          <Input id="angleStep" className="h-7 w-24 border-slate-300" />
+        </div>
+        <div className="flex items-center justify-between gap-6">
+          <Label htmlFor="easingIn" className="w-fit">
+            Easing in
+          </Label>
+          <Slider id="easingIn" className="w-24 h-7" />
+        </div>
+        <div className="flex items-center justify-between gap-6">
+          <Label htmlFor="easingOut" className="w-fit">
+            Easing out
+          </Label>
+          <Slider id="easingOut" className="w-24 h-9" />
+        </div>
       </div>
     </div>
   );
