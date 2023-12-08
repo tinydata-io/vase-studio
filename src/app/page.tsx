b863e0b0605c1b71ac636fafec46140c505a574f
Anna Kozik <95181360+aniasgit@users.noreply.github.com>
2023-12-08 13:26:58 +0100

    Main page layout (#4)
    * feat: create main page layout
    
    * add tabs to property editor, correct tailwind config
    
    * ref: add changes styling changes to dialog, add changes to property editor
    
    * ref: add changes to page layout
    
    * ref: remove focus css properties from dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import logo from "../assets/vaseLogo.svg";
+import { PropertyEditor } from "@/components/PropertyEditor";
 
 export default function Home() {
   return (
-    <main className="flex h-full items-center justify-center p-12 pb-[88px]">
-      <div className="relative w-full h-3/5">
-        <Image src={logo} alt="Vase Studio Logo" fill />
+    <main className="flex justify-center h-full p-4 pb-[40px]">
+      <div className="flex w-full 2xl:w-[1500px]">
+        <div className="flex justify-center items-center w-3/5 bg-red-200">
+          <div className="relative flex justify-center items-center w-[calc(85vh)] 2xl:w-full aspect-square bg-green-200">
+            Place for vase image component
+          </div>
+        </div>
+        <div className="flex justify-center items-start w-2/5 pl-4">
+          {/* Place for Tabs Component */}
+          <PropertyEditor />
+        </div>
       </div>
     </main>
   );
